b2b75748f28ed3aeaf4fec4550667172c786fb62
Ilaria <43253244+ilariae@users.noreply.github.com>
2022-07-17 19:36:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -417,13 +417,280 @@ while isDetection:
 for image in saved_images:
   cv2_imshow(image)
 
+  
+img_path = "/content/gdrive/MyDrive/Computer Vision Ilaria/images/data1/"
+
+def resize_images(images, letter): 
+  sizes = [16, 224]
+  for id,image in enumerate(images):
+    for size in sizes:
+      file_name = letter.upper()+"_" + str(id) +"_"+  str(size) #give the pic a name
+      print(file_name) #print the name
+      try:
+        resized_img = cv2.resize(image, (size, size), interpolation = cv2.INTER_AREA) #resize image
+      except:
+        break
+      cv2_imshow(resized_img) #print resized pic
+      cv2.imwrite(img_path + file_name + ".jpg", resized_img) #save image to folder
+      
+     
+#calling the function, change the letter when needed
+resize_images(saved_images, 'N')
+
 # ----- Task 6 -----
 
+# changes the shape of the array of the 16x16 images and appends it into the text file dataset.txt
+def Resize_and_Write(id, letter, dataset):
+  #given an id and letter, reads the image and converts it into a list
+  img = cv2.imread(img_path +letter+"_" + str(id) + "_16.jpg") #
+  res = cv2.resize(img, dsize=(1,256), interpolation=cv2.INTER_CUBIC) # 16x16 to (1,256) 1row | 256 columns
+  flat_res = [str(a[0][0]) for a in res] # 1 with 256 elem  
+  is_empty = False
+  try:
+    with  open("/content/gdrive/MyDrive/Computer Vision Ilaria/" + dataset, "r") as fileobj: # open the dataset in read mode
+      if len(fileobj.readlines())==0: # checks if the dataset is empty
+        is_empty = True
+  except:
+    is_empty = True # if the dataset does not exist, set the empty variable to true
+  with open("/content/gdrive/MyDrive/Computer Vision Ilaria/" + dataset, "a+") as fileobj: # open the dataset in append mode
+    L_list=[letter, *flat_res] # create a list with the letter as first element, followed by all the values of the pixels
+    str_list = str(L_list)
+    str_list = str_list.replace('\'', '')[1:-1] # remove the quotes and the brackets
+    if is_empty:
+      fileobj.write(str_list)#Creates file if file doesnt exist/also appends 
+    else:
+      fileobj.write("\n" + str_list)#Creates file if file doesnt exist/also appends
+
+letters = ["M", "N", "W"]
+dataset_1 = "dataset1.txt"
+dataset_2 = "dataset2.txt"
+dataset_3 = "dataset3.txt"
+
+#change when needed
+n_images = 100
+
+#Below we just change the name of the dataset as needed.
+for i in range(n_images):
+  Resize_and_Write(i, "N", dataset_3)
+
+#At the end we shuffle the lines of the dataset.
+lines = open('/content/gdrive/MyDrive/Computer Vision Ilaria/dataset2.txt').readlines()
+random.shuffle(lines)
+open('/content/gdrive/MyDrive/Computer Vision Ilaria/dataset2.txt', 'w').writelines(lines)
+
+# ----- Task 7 -----
+
+dataset_file_path = "/content/gdrive/MyDrive/Computer Vision Ilaria/dataset1.txt"
+
+#The following function takes the dataset file and returns two arrays: samples and letters.
+def load_dataset(dataset_file_path):
+    a = np.loadtxt(dataset_file_path, delimiter=',', converters={ 0 : lambda ch : ord(ch)-ord('A') })
+    samples, letters = a[:,1:], a[:,0] # samples takes all the rows and all the columns except the first column
+    # letters: keep only the first column
+    return samples, letters
+
+#here we split the dataset for training and validation
+train_ratio = 0.7
+samples, letters = load_dataset(dataset_file_path)
+n_train_samples = int(len(samples) * train_ratio)
+x_train, y_train = samples[:n_train_samples], letters[:n_train_samples] # keeps only the first 70% of rows
+x_val, y_val = samples[n_train_samples:], letters[n_train_samples:] # keeps only the last 30%
+print(x_train.shape)
+
+num_classes = 26 # number of letters
+epochs = 100 # number of training s
 
 
+#here we split the dataset for training and validation
+train_ratio = 0.7
+samples, letters = load_dataset(dataset_file_path)
+n_train_samples = int(len(samples) * train_ratio)
+x_train, y_train = samples[:n_train_samples], letters[:n_train_samples] # keeps only the first 70% of rows
+x_val, y_val = samples[n_train_samples:], letters[n_train_samples:] # keeps only the last 30%
+
+x_train = x_train.astype('float32')
+x_val = x_val.astype('float32')
+x_train /= 255 # x_train=x_train/255    normalize the numbers in the interval [0,1]
+x_val /= 255
+print(x_train.shape[0], 'train samples') # print the number of images
+print(x_val.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = tf.keras.utils.to_categorical(y_train, num_classes) # converts the prediction letter into a number encoding for that letter
+y_val = tf.keras.utils.to_categorical(y_val, num_classes)
+
+model = Sequential() 
+model.add(Dense(100, activation='relu', input_shape=(256,))) # add a relu layer
+model.add(Dense(100, activation='relu')) # add anothe relu
+model.add(Dropout(0.4)) # add a regularizator
+model.add(Dense(100, activation='relu')) # add anothe relu
+model.add(Dense(100, activation='relu')) # add anothe relu 
+model.add(Dropout(0.4)) # add a regularizator
+model.add(Dense(num_classes, activation='softmax'))  # add a final softmax layer
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train,   
+                    epochs=epochs,
+                    verbose=1, # if verbose=1 print data for each epoch
+                    validation_data=(x_val, y_val)) # train the network
+score = model.evaluate(x_val, y_val, verbose=0) # evaluate the obtained model  on the validation set.  #model and dataset
+print('Validation loss:', score[0]) # print the loss
+print('Validation accuracy:', score[1]) # print the accuracy
+
+#After I trained the model, I save the model and weights in two files in the shared folder.
+model_json = model.to_json()
+with open("/content/gdrive/MyDrive/Computer Vision Ilaria/model1.json", "w") as json_file:
+    json_file.write(model_json)
+model.save_weights("/content/gdrive/MyDrive/Computer Vision Ilaria/model1_weights.h5")
+print("Saved model to disk")
+
+# ----- Comparison -----
+
+def evaluate_model(path, model):
+  train_ratio = 0.7
+  samples, letters = load_dataset(path) # load the dataset
+  n_train_samples = int(len(samples) * train_ratio)
+  x_train, y_train = samples[:n_train_samples], letters[:n_train_samples] # keeps only the first 70% of rows
+  x_val, y_val = samples[n_train_samples:], letters[n_train_samples:] # keeps only the last 30%
+  x_train = x_train.astype('float32')
+  x_val = x_val.astype('float32')
+  x_train /= 255 # x_train=x_train/255    normalize the numbers in the interval [0,1]
+  x_val /= 255
+  
+  print(x_train.shape[0], 'train samples') # print the number of images
+  print(x_val.shape[0], 'test samples')
+
+  # convert class vectors to binary class matrices
+  y_train = tf.keras.utils.to_categorical(y_train, num_classes) # converts the prediction letter into a number encoding for that letter
+  y_val = tf.keras.utils.to_categorical(y_val, num_classes)
+
+  score = model.evaluate(x_val, y_val, verbose=0) # evaluate the model on the validation set.  #model and dataset
+  print('Validation loss:', score[0]) # print the loss
+  print('Validation accuracy:', score[1]) # print the accuracy
+  
+  from keras.models import model_from_json
+g_path = "/content/gdrive/MyDrive/Computer Vision Ilaria/"
+# load json and create model
+json_file = open(g_path+'model3.json', 'r') 
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+
+loaded_model.summary()
+# load weights into new model
+loaded_model.load_weights(g_path+"model3_weights.h5")
+
+loaded_model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+dataset_file_paths = [g_path+"dataset1.txt", g_path+"dataset2.txt", g_path+"dataset3.txt"]
+for path in dataset_file_paths:
+  print(path)
+  evaluate_model(path, loaded_model)
+  
+# ----- Task 8 -----
+  
+# load json and create model
+json_file = open('/content/gdrive/MyDrive/Computer Vision Ilaria/model1.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json) #not used
+# load weights into new model
+loaded_model.load_weights("/content/gdrive/MyDrive/Computer Vision Ilaria/model1_weights.h5")
 
 
+prediction = loaded_model.predict(x_train[0:1,:]) # where hand_image is the probability image of your hand of size (1,256)
+prediction = prediction.argmax()
+predicted_letter = chr(ord('A') + prediction)
+print(predicted_letter)
 
+VideoCapture()
+eval_js('create()')
 
+CascadeFace = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+isFaceDetect=False
+margin = 30
+restr = 30
+n_images = 0
+Time = 1
+Picture_count = 1000
+while not isFaceDetect:
+  bigrect=[] 
+  byte = eval_js('capture()')
+  im = byte2image(byte)
+  gray = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY) # convert the image into GRAY
+  hsv =  cv2.cvtColor(im, cv2.COLOR_RGB2HSV) #convert image to color
+  face = detect(gray, cascade = CascadeFace) # face contains the rectangle
+ 
+  if len(face)==0:
+    isFaceDetect=False
+  else:
+    isFaceDetect=True
+    
+  if isFaceDetect: # if a face is found
+    restricted_face = restrict(face[0])
+    hsv_roi=hsv[restricted_face[1]:restricted_face[3], restricted_face[0]:restricted_face[2]] # !!! better to restrict this face for the histogram
+    mask_roi = cv2.inRange(hsv_roi, np.array((0., 60., 32.)), np.array((180., 255., 255.)))
+    bigrect.append(compute_big_rect2(face[0])) # compute bigrect and add to the list
+  
+    isDetection=True # set a flag to start camshift
+    #gray_roi = gray[bigrect[0][1]:bigrect[0][3], bigrect[0][0]:bigrect[0][2]] # compute the region of interest
+    hist=cv2.calcHist([hsv_roi], [0], mask_roi, [180], [0,180]) # compute the histogram
+    hist=cv2.normalize(hist, hist,  alpha=0, beta=255, norm_type=cv2.NORM_MINMAX) # normalize the histogram 
+    plt.hist(hsv_roi.ravel(),256,[0,256]) # show the histogram
+    plt.show()
+
+    track_window = (bigrect[0][0], bigrect[0][1], bigrect[0][2], bigrect[0][3]) # compute the region of interest (bigrect)
+
+term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 ) # sets some criteria for camshift
+
+while isDetection:
+  byte = eval_js('capture()')
+  im = byte2image(byte)
+  hsv =  cv2.cvtColor(im, cv2.COLOR_RGB2HSV) #convert image to color
+  mask = cv2.inRange(hsv, np.array((0,64,32)), np.array((180,200,200)))
+  raw_im = cv2.cvtColor(gray,cv2.COLOR_GRAY2RGB)
+  prob = cv2.calcBackProject([hsv], [0], hist, [0, 180], 1) # compute the probability matrix 
+
+  for i in range(bigrect[0][0], bigrect[0][2]): # iterates between x1, x2
+    for j in range(bigrect[0][1], bigrect[0][3]): # iterates between y1, y2
+      prob[j,i]=0 # sets probability matrix to 0 on all points inside bigrect
+
+  prob &= mask
+
+  track_box, track_window = cv2.CamShift(prob, track_window, term_crit) # perform camshift over the track_window 
+  im[:] = prob[...,np.newaxis] # add probability matrix to the image
+  cv2.ellipse(im, track_box, (255, 0, 0), 2) # draw the ellipse
+  
+  if n_images==Picture_count:
+    break
+  else:
+    time.sleep(Time)
+  #we crop the image to have only the hand
+  cropped_image = im[int(track_window[1]):int(track_window[1]+track_window[3]), int(track_window[0]):int(track_window[0]+track_window[2])]
+  
+  #save_image=input("Do you want to save the image Y or N: ")
+  resized_img = cv2.resize(cropped_image, (16, 16), interpolation = cv2.INTER_AREA)
+  resized_img = cv2.resize(resized_img, dsize=(1,256), interpolation=cv2.INTER_CUBIC)
+  resized_img = np.array([str(a[0][0]) for a in resized_img])
+  resized_img=resized_img.astype('float32')
+  resized_img /= 255.0
+  #print(resized_img[np.newaxis, :].shape)
+  prediction = loaded_model.predict(resized_img[np.newaxis, :]) # where hand_image is the probability image of your hand of size (1,256)
+  prediction = prediction.argmax()
+  predicted_letter = chr(ord('A') + prediction)
+
+  font = cv2.FONT_HERSHEY_SIMPLEX
+  cv2.putText(im, predicted_letter, (int(track_window[0]), int(track_window[1])), font, 1, (0, 255, 255), 2, cv2.LINE_4)
+
+  eval_js('showimg("{}")'.format(image2byte(im)))
+  n_images+=1
 
 
